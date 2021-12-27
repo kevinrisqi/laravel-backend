1291f267d6843c66fabdd8b9f665de0a2a2958b3
kevinrisqi <kevinrisqi22@gmail.com>
2021-12-27 16:03:20 +0700

    Make all models

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $tags = $request->input('tags');
+        $categories = $request->input('categories');
+
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+
+        if ($id)
+        {
+            $product = Product::with(['category', 'galleries'])->find($id);
+        }
+    }
+}
