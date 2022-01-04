acab7c28a7c5d967b7cd7c37d2b1d0bfb85a15e4
kevinrisqi <kevinrisqi22@gmail.com>
2022-01-04 12:07:01 +0700

    Create product & product category controller

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -20,6 +20,6 @@ class ProductCategory extends Model
     ];
 
     public function products(){
-        return $this->hasMany(Transaction::class, 'categories_id', 'id');
+        return $this->hasMany(Product::class, 'categories_id', 'id');
     }
 }
