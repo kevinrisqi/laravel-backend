14a3503e64ec9cf5df3fcacc23bf3b84449c9ba1
kevinrisqi <kevinrisqi22@gmail.com>
2022-02-23 14:48:29 +0700

    Make register, login, user, editProfile, logout API

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
 
-Route::get('register', [UserController::class, 'register']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+});
